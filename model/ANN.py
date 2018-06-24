20fc9bb3d9205b53d386d7872ff530010a3c0459
kunyang <kun.bj@foxmail.com>
2018-06-24 09:39:17 -0400

    add model directory

---
+++ model/ANN.py
@@ -0,0 +1,18 @@
+"""
+
+    @ use ANN to classify
+"""
+from torch.nn import Linear
+
+__author__= 'Learn_and_live'
+
+
+
+class ANN():
+
+    def __init__(self):
+
+
+        self.layer= Linear()
+
+
