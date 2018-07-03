d88aaec46f639ca5dba10a397d3655a754586243
Learn-Live <kun.bj@foxmail.com>
2018-07-02 20:18:50 -0400

    add 'descriptions' in README

--- model/LSTM-Many2One.py
+++ model/LSTM-Many2One.py
@@ -61,29 +61,29 @@ def one_hot_sklearn(label_integer):
 
     return np.array(onehot_encoded, dtype=int)
 
-
 #
-# # Prepare data:
+# #
+# # # Prepare data:
+# #
+# # def prepare_sequence(seq, to_ix):
+# #     idxs = [to_ix[w] for w in seq]
+# #     return torch.tensor(idxs, dtype=torch.long)
+# #
+# #
+# # training_data = [
+# #     ("The dog ate the apple".split(), ["DET", "NN", "V", "DET", "NN"]),
+# #     ("Everybody read that book".split(), ["NN", "V", "DET", "NN"])
+# # ]
+# word_to_ix = {}
+# # for sent, tags in training_data:
+# #     for word in sent:
+# #         if word not in word_to_ix:
+# #             word_to_ix[word] = len(word_to_ix)
+# print('word_to_ix:', word_to_ix)
+# tag_to_ix = {"DET": 0, "NN": 1, "V": 2}
 #
-# def prepare_sequence(seq, to_ix):
-#     idxs = [to_ix[w] for w in seq]
-#     return torch.tensor(idxs, dtype=torch.long)
-#
-#
-# training_data = [
-#     ("The dog ate the apple".split(), ["DET", "NN", "V", "DET", "NN"]),
-#     ("Everybody read that book".split(), ["NN", "V", "DET", "NN"])
-# ]
-word_to_ix = {}
-# for sent, tags in training_data:
-#     for word in sent:
-#         if word not in word_to_ix:
-#             word_to_ix[word] = len(word_to_ix)
-print('word_to_ix:', word_to_ix)
-tag_to_ix = {"DET": 0, "NN": 1, "V": 2}
-
-# These will usually be more like 32 or 64 dimensional.
-# We will keep them small, so we can see how the weights change as we train.
+# # These will usually be more like 32 or 64 dimensional.
+# # We will keep them small, so we can see how the weights change as we train.
 
 
 ######################################################################
